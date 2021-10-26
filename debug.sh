98806eb6e159ce62bbf111f37d8bfce35a52bcd8
Cherny D. C. Berbesí I <cherny.berbesi@protonmail.com>
2021-10-26 16:07:19 -0600

    chore: all to main

--- debug.sh
+++ debug.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 set -e
-cd $(dirname $0)
-source common.sh
 
-docker build --pull -t $image_name .
+docker build -t flight_finder_logger_python:local .
 
 docker run --rm -it -p 9000:8080 \
-  -e "DEVELOPER_MODE=true" \
-  -e "LOG_LEVEL=INFO" \
-  -e "FLASK_APP=main.py" \
-  -e "FLASK_DEBUG=1" \
-  -e "WERKZEUG_DEBUG_PIN=off" \
-  -e "CLIENT_ID=clientid" \
-  -e "CLIENT_SECRET=secret" \
-  -e "ACCESS_TOKEN=token" \
-  -e "TOKEN_ISSUED_DATE=2021-10-15 16:00:28.816581" \
-  -e "TOKEN_EXPIRES_IN=1799" \
-  $image_name:latest \
+  flight_finder_logger_python:local \
   app.handler
